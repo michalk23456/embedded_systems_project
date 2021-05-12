36c2f4724c370e647a52b7c16264519265057a89
michalk23456 <83815439+michalk23456@users.noreply.github.com>
2021-05-12 10:52:19 +0200

    Add files via upload

---
+++ Lekcja 2/main.c
@@ -0,0 +1,17 @@
+
+int main()
+{
+  int counter =0;
+  while( counter < 20)
+  {
+    ++counter;
+    if (( counter & 1) != 0)
+    {
+        /* cos siewykona */
+    }
+    else
+    { /*cos innego sie wykona*/
+    }
+       
+  return 0;
+}

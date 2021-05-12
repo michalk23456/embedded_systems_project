78bf75f3023ed4d00d7bc6e223e510c48d788c17
michalk23456 <83815439+michalk23456@users.noreply.github.com>
2021-05-12 10:45:41 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,21 +1,17 @@
 
 int main()
 {
-  signed int counter =0;
-  ++counter;
-  ++counter;
-  ++counter;
-  ++counter;
-  ++counter;
-  ++counter;
-  ++counter;
-  ++counter;
-  ++counter;
-  ++counter;
-  ++counter;
-  ++counter;
-  ++counter;
-  ++counter;
-  ++counter;
+  int counter =0;
+  while( counter < 20)
+  {
+    ++counter;
+    if (( counter & 1) != 0)
+    {
+        /* cos siewykona */
+    }
+    else
+    { /*cos innego sie wykona*/
+    }
+       
   return 0;
 }
